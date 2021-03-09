18bbfd63f00c87dd1bdf1e351a400072909fca29
mehdimunim <68103264+mehdimunim@users.noreply.github.com>
2021-03-09 19:55:08 +0100

    Add

--- src/main/java/com/example/jetty_jersey/dao/Flight.java
+++ src/main/java/com/example/jetty_jersey/dao/Flight.java
@@ -1,9 +1,7 @@
 package com.example.jetty_jersey.dao;
 
-import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.Month;
 import java.util.ArrayList;
 import java.util.List;
 
