279409f81987bc7a038deb039f7c3c66d4db2bf9
hassnabdl <77720187+hassnabdl@users.noreply.github.com>
2021-03-13 11:13:02 +0100

    Update FlightResource.java

--- src/main/java/com/example/jetty_jersey/ws/flightResource.java
+++ src/main/java/com/example/jetty_jersey/ws/flightResource.java
@@ -1,8 +1,7 @@
 package com.example.jetty_jersey.ws;
 
-
-import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.Month;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,174 +11,148 @@ import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-import com.example.jetty_jersey.ws.Passenger;
-import com.example.jetty_jersey.ws.ExampleResource.ExampleClass;
+import com.example.jetty_jersey.dao.Flight;
 
-@Path("/flightResource")
-public class flightResource {
-	
+@Path("/FlightResource")
+public class FlightResource {
 
-	public static class Aircraft {
-		public String tailnumber;
-	}
-	
-	//seulement ces 2 champs pour simplifier le bouchon
-	public static class Flight {
-		public int id;
+	public static class flightsFromCriteria {
 		public String departure_aerodrome;
+		public LocalDateTime departureDateTime;
+		public LocalDateTime arrivalDateTime;
 	}
-	
-	public static class flightsWished {
-		public String departure_aerodrome;
-		public LocalDate from;
-		public LocalDate to;
-	}
-	
-	public static class flightInfo {
-		public LocalDateTime duration;
-		public int availabePlaces;
-		public double price; 
-		public String meeting_place; 
-		public String departure_aerodrome; 
-		public String destination_aerodrome; 
-		public LocalDateTime departureTime; 
-		public LocalDate departureDate; 
-		public LocalDate arrivalDate; 
-		public LocalDateTime arrivalTime; 
-		public Pilot pilot; 
-		public List<Passenger> passengers = new ArrayList<Passenger>() ; 
-		public int id;		
-	}
-	
-	
 
+	// return information of a specific flight (from its ID)
+	@GET
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Path("/{id}")
+	public Flight getFlightInfo(@PathParam("id") int flightId) {
+
+		// Flight flight = DAO.getFlight(flightId);
+		// if (flight == null){ throw new NotFoundException(); }
+
+		List<Flight> flightsList = new ArrayList<Flight>();
+
+		Flight f1 = new Flight();
+		f1.id = 1234;
+		f1.departure_aerodrome = "London";
+		flightsList.add(f1);
+
+		Flight f2 = new Flight();
+		f2.id = 123;
+		f2.departure_aerodrome = "Marseille";
+		flightsList.add(f2);
+
+		Flight f3 = new Flight();
+		f3.id = 12;
+		f2.departure_aerodrome = "Paris";
+		flightsList.add(f3);
+
+		for (Flight flight : flightsList) {
+			if (flightId == flight.id) {
+				return flight;
+			}
+		}
+		return null;
+	}
+
+	// returns flights based on specific criteria (departure aerodrome, desired
+	// period)
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	@Path("/aircraft")
-	public List<Aircraft> getAircraft() {
-		 List<Aircraft>  fleet = new ArrayList<Aircraft>();
-		 
-		 Aircraft a = new Aircraft();
-		 a.tailnumber ="F5GHA";
-		 fleet.add(a);
-		
-		 Aircraft b = new Aircraft();
-		 a.tailnumber ="F6ZBA";
-		 fleet.add(b);
+	@Path("/search-flight/flights")
+	public List<flightsFromCriteria> getFlighsFromCriteria() {
+		List<flightsFromCriteria> flightsList = new ArrayList<flightsFromCriteria>();
 
-		return fleet;
-	}
-	
-	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	@Path("/flight-sharing/flights")
-	public List<Flight> getFlight() {
-		List<Flight>  fleet = new ArrayList<Flight>();
-		 
-		 Flight f1 = new Flight();
-		 f1.id = 124;
-		 f1.departure_aerodrome = "London";
-		 fleet.add(f1);
-		
-		 Flight f2 = new Flight();
-		 f2.id = 255;
-		 f2.departure_aerodrome = "Marseille";
-		 fleet.add(f2);
+		flightsFromCriteria f1 = new flightsFromCriteria();
+		f1.departure_aerodrome = "London";
+		f1.departureDateTime = LocalDateTime.of(2020, Month.MAY, 25, 19, 30);
+		f1.arrivalDateTime = LocalDateTime.of(2020, Month.MAY, 25, 20, 30);
+		flightsList.add(f1);
 
-		return fleet;
+		flightsFromCriteria f2 = new flightsFromCriteria();
+		f2.departure_aerodrome = "Marseille";
+		f2.departureDateTime = LocalDateTime.of(2020, Month.JANUARY, 25, 14, 30);
+		f2.arrivalDateTime = LocalDateTime.of(2020, Month.JANUARY, 25, 15, 30);
+		flightsList.add(f2);
+
+		return flightsList;
 	}
-	
-	
+
+	// edit information of a specific flight (from its ID)
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Path("/{id}")
+	public void editFlight(@PathParam("id") int flightId) {
+		// Flight flight = DAO.getFlight(flightId);
+		// if (flight == null){ throw new NotFoundException();
+		List<Flight> flightsList = new ArrayList<Flight>();
+
+		Flight f1 = new Flight();
+		f1.id = 1234;
+		f1.departure_aerodrome = "London";
+		flightsList.add(f1);
+
+		Flight f2 = new Flight();
+		f2.id = 123;
+		f2.departure_aerodrome = "Marseille";
+		flightsList.add(f2);
+
+		for (Flight flight : flightsList) {
+			if (flightId == flight.id) {
+				System.out.println("flight information has been changed. Flight ID :  " + flightId
+						+ " Departure Aerodrome : " + flight.departure_aerodrome);
+			}
+		}
+	}
+
+	// addition of a flight in the database by the pilot
 	@PUT
 	@Produces(MediaType.APPLICATION_JSON)
-	@Path("/flight-sharing/sign-in/pilot/{pilotId}/plane/{planeId}/flight/{flightId}")
-	public void addFlight(Flight flight) {
-		 flight.id = 124;
-		 flight.departure_aerodrome = "London";
+	@Path("/{pilotId}")
+	public void addFlight(@PathParam("pilotId") int pilotId) {
+		Flight flight = new Flight();
+		flight.id = 124;
+		flight.departure_aerodrome = "London";
+		System.out.println("The flight has been added successfully !");
 
 	}
-	
+
+	// Delete a specific flight (from its ID)
 	@DELETE
 	@Consumes(MediaType.APPLICATION_JSON)
-	@Path("/flight-sharing/sign-in/pilot/{pilotId}/edit-flight/plane/{planeId}/flight/{flightId}")
-	public void deleteFlight(Flight flight) {
-		System.out.println("this flight was deleted");
-	}
-	
-	@POST
-	@Consumes(MediaType.APPLICATION_JSON)
-	@Path("/flight-sharing/pilot/{pilotId}/flight/{flightId}/booking/{bookingId}/information")
-	public void changeFlightInfo(Flight flight) {
-		System.out.println("flight information has been changed. Flight ID :  " + flight.id + "Departure Aerodrome : " + flight.departure_aerodrome);
-	}
-	
-	
-	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	@Path("/flight-sharing/pilot/{pilotId}/flightPlanned")
-	public List<Flight> getFlighPlanned() {
-		 List<Flight>  fleet = new ArrayList<Flight>();
-		 
-		 Flight f1 = new Flight();
-		 f1.id = 124;
-		 f1.departure_aerodrome = "London";
-		 fleet.add(f1);
-		
-		 Flight f2 = new Flight();
-		 f2.id = 255;
-		 f2.departure_aerodrome = "London";
-		 fleet.add(f2);
+	@Path("/{id}")
+	public void deleteFlight(@PathParam("id") int flightId) {
 
-		return fleet;
-	}
-	
+		// Flight flight = DAO.getFlight(flightId);
+		// if (flight == null){ throw new NotFoundException(); }
 
-	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	@Path("/flight-sharing/search-flight/flights")
-	public List<flightsWished> getFlighsWished() {
-		 List<flightsWished>  fleet = new ArrayList<flightsWished>();
-		 
-		 flightsWished f1 = new flightsWished();
-		 f1.departure_aerodrome = "London";
-		 f1.from = LocalDate.of(2017, 1, 13);
-		 f1.to =  LocalDate.of(2017, 1, 13); 
-		 fleet.add(f1);
-		 
-		 flightsWished f2 = new flightsWished();
-		 f1.departure_aerodrome = "Marseille";
-		 f1.from = LocalDate.of(2020, 2, 12);
-		 f1.to =  LocalDate.of(2020, 2, 12); 
-		 fleet.add(f2);
-			
-		return fleet;
-	}
-	
-	
-	@POST
-	@Produces(MediaType.APPLICATION_JSON)
-	@Path("/flight-sharing/pilot/{pilotId}/flight/{flightId}/information")
-	public void getFlighInfo(Flight flight) {
-		 List<flightInfo>  fleet = new ArrayList<flightInfo>();
-		
-		 flightInfo f1 = new flightInfo();
-		 	f1.duration = LocalDateTime.of(2019, 03, 28, 14, 33, 48, 123456789);
-			f1.availabePlaces = 6;
-			f1.price = 250; 
-			f1.meeting_place = "Marseille"; 
-			f1.departure_aerodrome = "Marseille Aeroport M1"; 
-			f1.destination_aerodrome = "Marseille Aeroport M6"; 
-			f1.departureTime =LocalDateTime.of(2019, 03, 28, 14, 33, 48, 123456789); 
-			f1.departureDate = LocalDate.of(2020, 2, 12); ; 
-			f1.arrivalDate = LocalDate.of(2020, 2, 12);
-			f1.arrivalTime = LocalDateTime.of(2019, 03, 28, 14, 33, 48, 123456789); 
-			f1.id = 233;
-			fleet.add(f1);
-		System.out.println("All flight information");
-	}
+		List<Flight> flightsList = new ArrayList<Flight>();
 
+		Flight f1 = new Flight();
+		f1.id = 1234;
+		f1.departure_aerodrome = "London";
+		flightsList.add(f1);
 
+		Flight f2 = new Flight();
+		f2.id = 123;
+		f2.departure_aerodrome = "Marseille";
+		flightsList.add(f2);
+
+		Flight f3 = new Flight();
+		f3.id = 12345;
+		f2.departure_aerodrome = "Paris";
+		flightsList.add(f3);
+
+		for (Flight flight : flightsList) {
+			if (flightId == flight.id) {
+				flightsList.remove(flight);
+				System.out.println("The flight " + flightId + " has been deleted");
+			}
+		}
+	}
 }
